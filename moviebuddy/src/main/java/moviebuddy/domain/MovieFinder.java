b4c7577faf043c3509d2a1751a8f3f5944c93f19
jins <djseo9812@gmail.com>
2025-05-23 21:03:41 +0900

    Include moviebuddy directory as part of the project

---
+++ moviebuddy/src/main/java/moviebuddy/domain/MovieFinder.java
@@ -0,0 +1,53 @@
+package moviebuddy.domain;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import moviebuddy.ApplicationException;
+import moviebuddy.util.FileSystemUtils;
+
+public class MovieFinder {
+	
+	private MovieReader movieReader = new CsvMovieReader();
+	
+
+    /**
+     * 저장된 영화 목록에서 감독으로 영화를 검색한다.
+     * 
+     * @param directedBy 감독
+     * @return 검색된 영화 목록
+     */
+    public List<Movie> directedBy(String directedBy) {
+        return movieReader.loadMovies().stream()
+                           .filter(it -> it.getDirector().toLowerCase().contains(directedBy.toLowerCase()))
+                           .collect(Collectors.toList());
+    }
+
+    /**
+     * 저장된 영화 목록에서 개봉년도로 영화를 검색한다.
+     * 
+     * @param releasedYearBy
+     * @return 검색된 영화 목록
+     */
+    public List<Movie> releasedYearBy(int releasedYearBy) {
+        return movieReader.loadMovies()
+        				   .stream()
+                           .filter(it -> Objects.equals(it.getReleaseYear(), releasedYearBy))
+                           .collect(Collectors.toList());
+    }
+
+    
+    
+
+}
