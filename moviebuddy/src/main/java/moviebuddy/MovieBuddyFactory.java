26fe8c4954e75a1854ba1bbd60ba1809104345d7
jins <djseo9812@gmail.com>
2025-05-28 13:21:48 +0900

    section6

--- moviebuddy/src/main/java/moviebuddy/MovieBuddyFactory.java
+++ moviebuddy/src/main/java/moviebuddy/MovieBuddyFactory.java
@@ -1,17 +1,37 @@
 package moviebuddy;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.Scope;
 
 import moviebuddy.domain.CsvMovieReader;
 import moviebuddy.domain.MovieFinder;
+import moviebuddy.domain.MovieReader;
 
 @Configuration
+@Import({ MovieBuddyFactory.DomainModuleConfig.class, MovieBuddyFactory.DataSourceModuleConfig.class })
 public class MovieBuddyFactory {
+
+	@Configuration
+	static class DomainModuleConfig{
+		
+		@Bean
+		public MovieFinder movieFinder(MovieReader movieReader) {
+			return new MovieFinder(movieReader);
+		}
+		
+	}
 	
-	@Bean
-	public MovieFinder movieFinder() {
-		return new MovieFinder(new CsvMovieReader());
+	@Configuration
+	static class DataSourceModuleConfig{
+		
+		@Bean
+		public MovieReader movieReader() {
+			return new CsvMovieReader();
+		}
 	}
 
 }
